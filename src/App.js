8105f98f70588d4aff8ad6b61db21c732fd46887
Tomasz Wachnik <tomasz.wachnik@gmail.com>
2021-05-30 23:25:32 +0200

    React app test

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = () => (
+  <div className="wrapper">
+    <h1 className="mainHeader">App</h1>
+    <h2 className="secondaryHeader">test</h2>
+  </div>
+);
+
+export default App;
