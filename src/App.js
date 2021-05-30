a9fc6b0bc1296fbe214277d7648fabe63ffaed28
Tomasz Wachnik <tomasz.wachnik@gmail.com>
2021-05-30 23:40:06 +0200

    Test

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => (
   <div className="wrapper">
     <h1 className="mainHeader">App</h1>
-    <h2 className="secondaryHeader">test</h2>
+    <h2 className="secondaryHeader">Test</h2>
   </div>
 );
 
